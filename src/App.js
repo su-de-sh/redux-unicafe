08c48fcf792a4a7d98757ec16af89484fb7e5c10
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-22 21:01:13 +0545

    feat: code refactoring

---
+++ src/App.js
@@ -0,0 +1,21 @@
+const App = () => {
+  const good = () => {
+    store.dispatch({
+      type: "GOOD",
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button>ok</button>
+      <button>bad</button>
+      <button>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok</div>
+      <div>bad</div>
+    </div>
+  );
+};
+
+export default App;
